69d4913d167946cd645a9749c74ac98daf503b84
Vitaliy <pavlus.vit@gmail.com>
2024-03-20 17:01:44 +0200

    add dynamodb

--- main.tf
+++ main.tf
@@ -1,8 +1,27 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "058264303862-my-tf-test-bucket"
+locals {
+  tag_name = var.use_locals ? "forum" : var.bucket_name
+}
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+module "s3" {
+  source = "./modules/s3"
+  bucket_name = "${data.aws_caller_identity.current.account_id}-my-tf-test-bucket-new-module-${data.aws_caller_identity.current.account_id}"
+}
+
+module "table_authors" {
+  source = "./modules/dynamodb"
+  context = module.label.context
+  name = "authors"
+}
+
+module "table_courses" {
+  source = "./modules/dynamodb"
+  context = module.label.context
+  name = "courses"
+}
+
+resource "aws_s3_bucket" "this" {
+  # bucket = "058264303862-my-tf-test-bucket-new"
+  bucket = module.label_s3.id
+
+tags = module.label_s3.tags
 }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+    type = string
+    # default = "058264303862-my-tf-test-bucket-new"
+}
+
+variable "use_locals" {
+    type = bool
+    # default = "058264303862-my-tf-test-bucket-new"
+}
